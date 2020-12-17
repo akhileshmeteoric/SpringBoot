1e356f919aa8a9241e205cc71ffa05a5af8e6c42
Akhilesh <Akhilesh@DESKTOP-N6ESK7I>
2020-12-17 23:00:53 +0530

    Lab : 1 Committed

---
+++ Lab1/src/main/java/com/akhilesh/springboot/JDBCCustomerDAO.java
@@ -0,0 +1,27 @@
+package com.akhilesh.springboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JDBCCustomerDAO implements CustomerDAO {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void addCustomer(Customer cust) {
+		String sql = "insert into customers values(?,?,?,?,?)";
+		jdbcTemplate.update(sql, cust.getCid(), cust.getCname(), cust.getEmail(), cust.getPhone(), cust.getCid());
+	}
+
+	@Override
+	public List<Customer> getAllCustomer() {
+
+		String sql = "select * from customers";
+		List<Customer> list = jdbcTemplate.query(sql, new CustomerRowMapper());
+		return list;
+	}
+
+}
---
+++ Lab1/src/main/java/com/akhilesh/springboot/Lab1.java
@@ -0,0 +1,23 @@
+package com.akhilesh.springboot;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Lab1 {
+public static void main(String[] args) {
+	
+	AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext(JLCConfig.class);
+	CustomerDAO cdao=(CustomerDAO)ctx.getBean("custDAO");
+	
+	//1. addCustomer
+	Customer cust1= new Customer(101, "Akhilesh", "akhilesh@gmail.com", 9999, "Blore");
+	cdao.addCustomer(cust1);
+	
+	//2. getAllCustomer
+	
+	System.out.println("getAllCustomer");
+	List<Customer> list = cdao.getAllCustomer();
+	list.forEach(cust -> System.out.println(cust));
+}
+}
