002887b9da45baf70b7d797acf0b29673c355a78
Akhilesh <Akhilesh@DESKTOP-N6ESK7I>
2020-12-18 03:23:35 +0530

    Config class name updated

--- Lab1/src/main/java/com/akhilesh/springboot/Lab1.java
+++ Lab1/src/main/java/com/akhilesh/springboot/Lab1.java
@@ -7,11 +7,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Lab1 {
 public static void main(String[] args) {
 	
-	AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext(JLCConfig.class);
+	AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext(MyConfig.class);
 	CustomerDAO cdao=(CustomerDAO)ctx.getBean("custDAO");
 	
 	//1. addCustomer
-	Customer cust1= new Customer(101, "Akhilesh", "akhilesh@gmail.com", 9999, "Blore");
+	Customer cust1= new Customer(113, "Akhilesh", "akhilesh@gmail.com", 9999, "Blore");
 	cdao.addCustomer(cust1);
 	
 	//2. getAllCustomer
---
+++ Lab1/src/main/java/com/akhilesh/springboot/MyConfig.java
@@ -0,0 +1,38 @@
+package com.akhilesh.springboot;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class MyConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/jlcdb");
+		ds.setUsername("root");
+		ds.setPassword("bangalore");
+		ds.setInitialSize(10);
+		ds.setMaxActive(15);
+		return ds;
+
+	}
+
+	@Bean
+	public JdbcTemplate jdbctemplate(DataSource ds) {
+
+		return new JdbcTemplate(ds);
+	}
+
+	@Bean(name = "custDAO")
+	public CustomerDAO customerDAO() {
+		return new JDBCCustomerDAO();
+
+	}
+
+}
