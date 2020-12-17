e1b9fcbb7d2f7cdd1e0df3b56266ec144e31374d
Akhilesh <Akhilesh@DESKTOP-N6ESK7I>
2020-12-17 23:06:04 +0530

    Lab2

---
+++ Lab2/src/main/java/com/akhilesh/springboot/MyBootApplication.java
@@ -0,0 +1,35 @@
+package com.akhilesh.springboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.akhilesh.springboot")
+public class MyBootApplication implements CommandLineRunner {
+
+	@Autowired
+	CustomerDAO cdao;
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		// 1. addCustomer
+		Customer cust1 = new Customer(102, "Guddu", "guddu@gmail.com", 8888, "Blore");
+		cdao.addCustomer(cust1);
+
+		// 2. getAllCustomer
+
+		System.out.println("getAllCustomer");
+		List<Customer> list = cdao.getAllCustomer();
+		list.forEach(cust -> System.out.println(cust));
+
+	}
+}
